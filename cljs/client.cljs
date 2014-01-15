51494db394d15628ab139ddb7327949edacd84a9
Tim Licata <tim@timothylicata.com>
2014-01-14 23:07:18 -0800

    get basic websocket communication setup

--- cljs/client.cljs
+++ cljs/client.cljs
@@ -10,6 +10,17 @@
     [:div
      [:h3 "ing"]]])))
 
+(def websocket-url "ws://localhost:8080/socky")
+(def websocket (js/WebSocket. websocket-url))
+
+(set! (.-onmessage websocket)
+      (fn [event]
+        (let [message (.-data event)]
+          (.log js/console message))))
+
+(defn send-message [msg]
+  (.send websocket (or msg "pizza")))
+
 (set! (.-onload js/window)
       #(d/replace-contents! (sel1 :#content)
                             (render-page)))
--- src/socky/handler.clj
+++ src/socky/handler.clj
@@ -12,8 +12,20 @@
     (include-js "/js/bin/main.js")]
    [:body [:div#content]]))
 
+(defn websocket-handler [request]
+  (httpkit/with-channel request channel
+    (httpkit/on-close channel (fn [status] (println "channel closed")))
+    (if (httpkit/websocket? channel)
+      (println "WebSocket channel")
+      (println "HTTP channel"))
+    (httpkit/on-receive channel
+                (fn [data]
+                  (println (str "on receive: " data))
+                  (httpkit/send! channel data)))))
+
 (defroutes app-routes
   (GET "/" [] (page-frame))
+  (GET "/socky" [] websocket-handler)
   (route/resources "/")
   (route/not-found "Not Found"))
 
