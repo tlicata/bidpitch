80bafe728436a0d13bea6f993d75dec3e514f668
Tim Licata <tim@timothylicata.com>
2014-01-20 20:53:25 -0800

    basic dealing functionality

--- src/socky/crossover/cards.clj
+++ src/socky/crossover/cards.clj
@@ -1,10 +1,7 @@
 (ns socky.crossover.cards)
 
-(def ranks [:2 :3 :4 :5 :6 :7 :8 :9 :10 :J :Q :K :A])
-(def suits [:S :C :H :D])
+(def ranks ["2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K" "A"])
+(def suits ["S" "C" "H" "D"])
 
-(defn deck []
-  (flatten (map #(map (fn [rank] {:rank rank :suit %}) ranks) suits)))
-
-(defn shuffled-deck []
-  (shuffle (deck)))
+(defn create-deck []
+  (shuffle (flatten (map #(map (fn [rank] (str rank %)) ranks) suits))))
--- src/socky/crossover/game.clj
+++ src/socky/crossover/game.clj
@@ -1,4 +1,5 @@
-(ns socky.crossover.game)
+(ns socky.crossover.game
+  (:require [socky.crossover.cards :refer [create-deck]]))
 
 (def player {:id nil :name ""})
 (def player-state {:cards [] :tricks []})
@@ -10,4 +11,27 @@
 
 (def table {:players [] :scores []})
 
-(defn state {:table table :round round})
+(def state {:table table :round round})
+
+(defn deal-cards [deck num-players]
+  (take num-players (partition 6 deck)))
+
+(defn deal [deck players]
+  (let [hands (deal-cards deck (count players))]
+    (map (fn [hand player]
+           {:id (:id player)
+            :cards hand
+            :tricks []})
+         hands players)))
+
+(def tim {:id 1 :name "Tim"})
+(def sharon {:id 2 :name "Sharon"})
+(def louise {:id 3 :name "Louise"})
+(def paul {:id 4 :name "Paul"})
+(def test-players [tim sharon louise paul])
+
+(def test-table {:players test-players :scores [3 5]})
+(def test-round {:dealer tim
+                 :player-states (deal (create-deck) test-players)
+                 :bids {}
+                 :trump ""})
