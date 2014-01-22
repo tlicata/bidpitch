ffa7a73eadb3507c3a3ca2ab2110c1bf3f1f0201
Tim Licata <tim@timothylicata.com>
2014-01-21 19:01:33 -0800

    start on om component lifecycle

--- cljs/client.cljs
+++ cljs/client.cljs
@@ -6,10 +6,6 @@
             [om.dom :as dom :include-macros true])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn widget [data owner]
-  (om/component
-   (dom/div nil "Hello world!")))
-
 (def websocket-url "ws://localhost:8080/socky")
 (def websocket (atom (chan)))
 
@@ -19,10 +15,24 @@
 
 (set! (.-onload js/window)
       (fn []
-        (om/root {} widget (.getElementById js/document "content"))
         (go
          (reset! websocket (<! (ws-ch websocket-url)))
-         (loop []
-           (when-let [msg (<! @websocket)]
-             (.log js/console (str (keys (read-string (:message msg)))))
-             (recur))))))
+         (om/root
+          {}
+          (fn [data owner]
+            (reify
+              om/IWillMount
+              (will-mount [_]
+                (go (while true
+                      (when-let [msg (<! @websocket)]
+                        (.log js/console "message received" (:message msg))
+                        (om/set-state! owner :message (:message msg))))))
+              om/IRender
+              (render [_]
+                (dom/div nil
+                         (let [msg (om/get-state owner :message)]
+                           (.log js/console "render")
+                           (if msg
+                             msg
+                             "Hello World"))))))
+         (.getElementById js/document "content")))))
