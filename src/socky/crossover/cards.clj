2d5f389f7ed17d9da96b889ba9afd5d3480c4eb2
Tim Licata <tim@timothylicata.com>
2014-02-07 21:34:40 -0800

    functions for getting suit and rank from a card

--- src/socky/crossover/cards.clj
+++ src/socky/crossover/cards.clj
@@ -3,5 +3,11 @@
 (def ranks ["2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K" "A"])
 (def suits ["S" "C" "H" "D"])
 
+(defn get-suit [card]
+  (str (second card)))
+
+(defn get-rank [card]
+  (str (first card)))
+
 (defn create-deck []
   (shuffle (flatten (map #(map (fn [rank] (str rank %)) ranks) suits))))
