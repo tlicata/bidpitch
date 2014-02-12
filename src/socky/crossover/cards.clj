5059ce09842f2c014f63cef417f593b7c33a70bf
Tim Licata <tim@timothylicata.com>
2014-02-11 23:13:07 -0800

    card ranks should be one char (10 -> T)

--- src/socky/crossover/cards.clj
+++ src/socky/crossover/cards.clj
@@ -1,6 +1,6 @@
 (ns socky.crossover.cards)
 
-(def ranks ["2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K" "A"])
+(def ranks ["2" "3" "4" "5" "6" "7" "8" "9" "T" "J" "Q" "K" "A"])
 (def suits ["S" "C" "H" "D"])
 
 (defn get-suit [card]
