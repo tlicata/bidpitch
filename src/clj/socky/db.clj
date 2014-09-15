2901be4826d9a975d9f108b50adc475c93200ede
Tim Licata <tim@timothylicata.com>
2014-09-15 15:08:57 -0700

    remove no longer needed migration functions

--- src/clj/socky/db.clj
+++ src/clj/socky/db.clj
@@ -4,24 +4,7 @@
 (def pg-db "postgresql://localhost:5432/bidpitch_dev")
 (def games-table "games")
 
-(def create-games-table-sql
-  (str "create table " games-table " (
-    id bigserial primary key,
-    name varchar(255) not null
-  );"))
-
 (defn game-add [title]
   (jdbc/insert! pg-db :games {:name title}))
 (defn game-all []
   (jdbc/query pg-db [(str "SELECT * FROM " games-table)]))
-
-(defn migrated? []
-  (-> (jdbc/query pg-db
-                 [(str "select count(*) from information_schema.tables "
-                       "where table_name='" games-table "'")])
-      first :count pos?))
-(defn migrate []
-  (when (not (migrated?))
-    (print "Creating database structure...") (flush)
-    (jdbc/execute! pg-db [create-games-table-sql])
-    (println " done")))
