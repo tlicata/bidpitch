ecb6027bb45cc37e3a1d58b5c6c7962dc5704651
Tim Licata <tim@timothylicata.com>
2014-09-28 10:26:10 -0700

    db functions for players

--- src/clj/socky/db.clj
+++ src/clj/socky/db.clj
@@ -1,5 +1,6 @@
 (ns socky.db
-  (:require [clojure.java.jdbc :as jdbc]))
+  (:require [cemerick.friend.credentials :as creds]
+            [clojure.java.jdbc :as jdbc]))
 
 (def pg-db "postgresql://localhost:5432/bidpitch_dev")
 (def games-table "games")
@@ -8,3 +9,15 @@
   (jdbc/insert! pg-db :games {:name title}))
 (defn game-all []
   (jdbc/query pg-db [(str "SELECT * FROM " games-table)]))
+
+(defn player-query []
+  (jdbc/query pg-db ["SELECT * FROM players"]))
+(defn player-get [name]
+  (jdbc/query pg-db ["SELECT * FROM players WHERE username = ?" name]
+              :result-set-fn first))
+(defn player-add [username pswd]
+  (let [password (creds/hash-bcrypt pswd)]
+    (jdbc/insert! pg-db :players {:username username
+                                  :password password})))
+(defn player-remove [name]
+  (jdbc/delete! pg-db :players ["username = ?" name]))
