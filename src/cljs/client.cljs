1b1e3280754b50ece230100eaa3d960ea1219d8a
Tim Licata <tim@timothylicata.com>
2015-03-08 15:18:20 -0700

    "One more time" -> "Play again!"

--- src/cljs/client.cljs
+++ src/cljs/client.cljs
@@ -129,7 +129,7 @@
                  (dom/button #js {:className "button"
                                   :style (display (game/game-over? data))
                                   :onClick #(send-message "start")}
-                             "One more time"))))))
+                             "Play again!"))))))
 
 (defn table-card-li [data owner]
   (dom/li nil (card-ui data)))
