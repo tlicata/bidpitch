fe884eeca8ce9cfd34bed38abdd2fe9994f02d1c
Tim Licata <tim@timothylicata.com>
2015-04-17 00:17:43 -0700

    upgrade clojure to 1.6.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject socky "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/java.jdbc "0.3.3"]
