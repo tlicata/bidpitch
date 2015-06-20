d4aa194ec71a9eb0b74b81acb8c4224b3661b05a
Tim Licata <tim@timothylicata.com>
2015-06-19 21:19:42 -0700

    render table-cards with flexbox in vanilla js

--- resources/public/js/src/table-view.js
+++ resources/public/js/src/table-view.js
@@ -2,8 +2,40 @@ goog.provide("socky.table");
 
 socky.table = (function () {
   return {
+    componentDidMount: function (om) {
+      var canvas = om.owner.getDOMNode();
+      console.log("componentDidMount", canvas);
+    },
     render: function (data) {
-      return React.createElement("canvas", null, "");
+      console.log("render", data);
+      var tableCards = data["table-cards"];
+      var players = data["players"];
+      var playerElements = [];
+      if (tableCards && players && players.map) {
+        playerElements = players.map(function (player, idx) {
+          var name = data.me === players[idx] ? "You" : players[idx];
+          var text = React.createElement("span", {}, name);
+          var cardOpts = {className: "card"};
+          if (tableCards.length > idx) {
+            cardOpts.src = "/img/cards/individual/" + tableCards[idx] + ".svg?2.09"
+          }
+          var card = React.createElement("img", cardOpts, []);
+          return React.createElement("div", {
+            style: {
+              alignItems: "center",
+              display: "flex",
+              flexDirection: "column"
+            }
+          }, [text, card]);
+        });
+      }
+      return React.createElement("div", {
+        style: {
+          alignItems: "flex-start",
+          display: "flex",
+          flexDirection: "row"
+        }
+      }, playerElements);
     }
   };
 })();
