0fad03f6511afb3f81e61e4697aab80b916b02ab
Tim Licata <tim@timothylicata.com>
2016-01-24 15:07:27 -0800

    ai plays with random valid bids and cards

--- src/clj/socky/ai.clj
+++ src/clj/socky/ai.clj
@@ -6,6 +6,19 @@
 (def to-server (atom (chan)))
 (def state (atom {}))
 
+(defn who-am-i [state]
+  (:me state))
+(defn my-turn? [state]
+  (= (game/get-onus state) (who-am-i state)))
+(defn my-turn-to-bid? [state]
+  (and (my-turn? state) (game/bidding-stage? state)))
+(defn my-turn-to-play? [state]
+  (and (my-turn? state) (not (game/bidding-stage? state))))
+(defn my-cards [state]
+  (try
+    (:cards (val (first (:player-cards state))))
+    (catch Exception _ [])))
+
 (defn play [in out]
   (reset! to-server in)
   (reset! from-server out)
@@ -13,7 +26,13 @@
   (let [jwt (<!! out) game-state (<!! out)]
     (>!! in {:message "join"})
     (loop []
-      (when-let [msg (<!! out)]
-        (reset! state (read-string msg))
+      (when-let [game-state (read-string (<!! out))]
+        (reset! state game-state)
+        (when (my-turn? game-state)
+          (Thread/sleep 2000)
+          (>!! in {:message
+                   (if (my-turn-to-bid? game-state)
+                     (str "bid:" (first (filter (partial game/valid-bid? game-state (who-am-i game-state)) [0 1 2 3 4])))
+                     (str "play:" (rand-nth (filter (partial game/valid-play? game-state (who-am-i game-state)) (my-cards game-state)))))}))
         (recur)))
     (println "AI stopped due to disconnect")))
