24d1641f572f2e0dc2979f720c0f337ad798086b
Tim Licata <tim@timothylicata.com>
2016-01-26 10:52:00 -0800

    pull setup into own fn and test dealer and onus

--- test/socky/test/ai.clj
+++ test/socky/test/ai.clj
@@ -3,28 +3,50 @@
   (:require [clojure.core.async :refer [<!! >!! chan]]
             [socky.game :as game]))
 
+(defn setup-game [my-name from-ai to-ai]
+  ;; Expect to receive "name" message from AI.  Every client
+  ;; tries to register with a name and is accepted if the name
+  ;; is available and rejected otherwise.
+  (is (= {:message my-name} (<!! from-ai)))
+
+  ;; We'll accept the name "AI". In this case the server usually
+  ;; sends back a signed JWT, giving this client the privilege to
+  ;; that name in case of future collisions. Really, we can send
+  ;; back anything other than "taken".
+  (>!! to-ai "your-name-was-accepted")
+
+  ;; After accepting the client, we send them the current state of
+  ;; the game (shielded so they can't see other players' cards).
+  (>!! to-ai game/empty-state)
+
+  ;; Expect to receive the "join" message from the client saying
+  ;; the AI wants to join the game.
+  (is (= {:message "join"} (<!! from-ai)))
+
+  ;; The client will now sit in a loop, waiting for updates from
+  ;; the server. The browser will show this new game state to the
+  ;; player and respond with player action. The AI will examine
+  ;; the state and try to come up with its own response (if it is
+  ;; its turn to play).
+)
+
 (deftest test-ai
   (testing "AI interaction with server"
     (let [my-name "AI"
           from-ai (chan) to-ai (chan)
-          state game/empty-state
           brain (future (play from-ai to-ai))]
 
-      ;; Expect to receive "name" message from AI.  Every client
-      ;; tries to register with a name and is accepted if the name
-      ;; is available and rejected otherwise.
-      (is (= {:message my-name} (<!! from-ai)))
+      (setup-game my-name from-ai to-ai)
 
-      ;; We'll accept the name "AI". In this case the server usually
-      ;; sends back a signed JWT, giving this client the privilege to
-      ;; that name in case of future collisions. Really, we can send
-      ;; back anything other than "taken".
-      (>!! to-ai "your-name-was-accepted")
-
-      ;; After accepting the client, we send them the current state of
-      ;; the game (shielded so they can't see other players' cards).
-      (>!! to-ai (prn-str (game/shield state my-name)))
-
-      ;; Expect to receive the "join" message from the client saying
-      ;; the AI wants to join the game.
-      (is (= {:message "join"} (<!! from-ai))))))
+      ;; Let's make up some states and see how the AI responds.
+      (let [opponent "opponent-name"
+            state (-> game/empty-state
+                      ;; Add the AI and another player
+                      (game/add-player my-name opponent)
+                      ;; Give them each some cards.
+                      (game/add-cards my-name ["AC" "KC" "JC"])
+                      (game/add-cards opponent ["2D" "4D" "6D"])
+                      ;; Set dealer and order players accordingly.
+                      (game/dealt-state))]
+        (is (= (game/get-dealer state) my-name))
+        (is (= (game/get-onus state) opponent))))))
