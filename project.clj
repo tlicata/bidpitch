65dd660355cec9268339e4330bf7d33b0da38ae6
Tim Licata <tim@timothylicata.com>
2017-02-07 20:40:47 -0800

    add project description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bidpitch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The card game"
+  :url "https://github.com/tlicata/bidpitch"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.48"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
