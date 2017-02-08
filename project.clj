57a5ed17240570a0b75bcaad7a022f6481f0fed8
Tim Licata <tim@timothylicata.com>
2017-02-07 20:30:28 -0800

    rename project to bidpitch

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject socky "0.1.0-SNAPSHOT"
+(defproject bidpitch "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0"]
