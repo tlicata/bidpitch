19adadd6a1fbad6e3814d692a7878771fe1f0de3
Tim Licata <tim@timothylicata.com>
2017-02-18 13:08:40 -0800

    upgrade clojurescript and lein-cljsbuild

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "The card game"
   :url "https://github.com/tlicata/bidpitch"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.48"]
+                 [org.clojure/clojurescript "1.9.473"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/core.memoize "0.5.8"]
                  [clj-jwt "0.1.0"]
@@ -12,7 +12,7 @@
                  [http-kit "2.1.16"]
                  [jarohen/chord "0.7.0-SNAPSHOT"]
                  [org.omcljs/om "0.8.8"]]
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
   :hooks [leiningen.cljsbuild]
   :main bidpitch.handler
   :source-paths ["src/clj" "src/cljc"]
