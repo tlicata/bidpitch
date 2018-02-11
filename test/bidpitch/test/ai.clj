fe898ba2f55020f42ec1e080fc99c69ec9383806
Tim Licata <tim@timothylicata.com>
2018-02-11 22:16:59 +0000

    Fixup specs from 88cfcdaa

--- test/bidpitch/test/ai.clj
+++ test/bidpitch/test/ai.clj
@@ -12,8 +12,8 @@
                   (game/add-cards opponent ["2D" "4D" "6D"])
                   (game/dealt-state))]
     (testing "possible bids"
-      (is (= (possible-moves state) [{:action "bid" :value 2}
-                                     {:action "bid" :value 0}]))
+      (is (= (possible-moves state) [{:action "bid" :value 0}
+                                     {:action "bid" :value 2}]))
       (is (= (possible-moves (-> state (game/bid opponent 0)))
              [{:action "bid" :value 2}])))
     (testing "possible cards"
